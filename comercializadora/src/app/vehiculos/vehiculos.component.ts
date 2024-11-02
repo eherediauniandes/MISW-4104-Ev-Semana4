eba752ad9a2ae7061859f4f7b4e61f242a3ef4db
Esteban Heredia <e.herediar@uniandes.edu.co>
2024-11-02 14:31:31 -0500

    Se calculan el total de vehiculos registrados por marca

--- comercializadora/src/app/vehiculos/vehiculos.component.ts
+++ comercializadora/src/app/vehiculos/vehiculos.component.ts
@@ -11,12 +11,27 @@ export class VehiculosComponent implements OnInit {
 
   constructor(private vehiculeService: VehiculosService) { }
   vehicles: Array<Vehiculo> = []
+  vehiclesByMarca: {[marca:string]:number} = {}
 
   getVehicles() {
     this.vehiculeService.getVehicles().subscribe(vehicles => {
       this.vehicles = vehicles;
-      console.log(this.vehicles);
+      this.calculateVehiclesByMarca();
     })
+  }
+
+  calculateVehiclesByMarca() {
+    this.vehiclesByMarca = {}; 
+    if (this.vehicles.length === 0) {
+      console.log('No se encontraron vehículos.'); // log
+    }
+    for (const vehicle of this.vehicles) {
+      if (this.vehiclesByMarca[vehicle.marca]) {
+        this.vehiclesByMarca[vehicle.marca]++;
+      } else {
+        this.vehiclesByMarca[vehicle.marca] = 1;
+      }
+    }
   }
 
   ngOnInit() {
