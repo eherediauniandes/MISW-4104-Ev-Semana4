71114f2fdc47ddf003244f07a88edacf12f2bf02
Esteban Heredia <e.herediar@uniandes.edu.co>
2024-11-02 13:58:43 -0500

    Se ajusta vista para parecer mas al mockup

--- comercializadora/src/app/app.module.ts
+++ comercializadora/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VehiculosModule } from './vehiculos/vehiculos.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      NavbarComponent,
+      BannerComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
